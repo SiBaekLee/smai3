68a65c016b9460ac1d15d1d92ea629277db27f98
jinhyeon <leeseeback1581@gmail.com>
2025-08-21 12:54:38 +0900

    0821

--- llm1/gemini2.py
+++ llm1/gemini2.py
@@ -3,7 +3,7 @@ from PIL import Image
 from myllm.myApi import geminiModel
 
 def test():
-    img = Image.open("img/iron.png")
+    img = Image.open("../img/iron.png")
     model = geminiModel()
     response = model.generate_content(["제시한 이미지를 3문장 이내의 한국어로 설명해 주세요",img])
     print(response.text)
