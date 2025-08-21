60bbfb84befaf4c87388ea27c5e1366560ba89ce
jinhyeon <leeseeback1581@gmail.com>
2025-08-21 13:28:23 +0900

    0821

---
+++ llm2/openai7.py
@@ -0,0 +1,18 @@
+from myllm.myApi import geminiModel, openAiModel, openAiModelArg, makeMsg
+
+
+def test(prompt):
+    openModel = openAiModel()
+    response = openModel.images.create_variation(
+        model="dall-e-2",
+        image=open(prompt, "rb"),
+        n=3,
+        size="1024x1024"
+    )
+    for n, data in enumerate(response.data):
+        print(n)
+        print(data.url)
+
+if __name__ == "__main__":
+    prompt = "img/amd.jpg"
+    test(prompt)
